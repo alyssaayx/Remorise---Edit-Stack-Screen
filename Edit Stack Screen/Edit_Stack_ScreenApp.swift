17fffeaedf0a61a14ecf4e44b96edae6de19fa1c
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-11-16 10:28:23 +0800

    Initial Commit

---
+++ Edit Stack Screen/Edit_Stack_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edit_Stack_ScreenApp.swift
+//  Edit Stack Screen
+//
+//  Created by rgs on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Edit_Stack_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
