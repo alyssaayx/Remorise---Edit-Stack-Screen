f9d0b5f30c7854d5fa82f10afa4ceb8d5e59604f
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-11-17 10:57:04 +0800

    The basics of the screen, TextField character limit is not set yet.

--- Edit Stack Screen/ContentView.swift
+++ Edit Stack Screen/ContentView.swift
@@ -7,10 +7,179 @@
 
 import SwiftUI
 
+
+
+
+
 struct ContentView: View {
+    @State private var text = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        NavigationView {
+            
+            VStack {
+                
+                ZStack(alignment: .topLeading) {
+                    Rectangle()
+                        .fill(Color(UIColor.systemTeal))
+                        .frame(height: 66)
+                    CustomTextEditor.init(placeholder: "Stack name", text: $text)
+                        .multilineTextAlignment(TextAlignment.leading)
+                        .frame(height: 66)
+                }
+                
+                
+                
+                
+                ScrollView(.horizontal) {
+                    HStack {
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "plus")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(30)
+                        }
+                        
+                        Button("#geography") {
+                            print("Pressed")
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                        
+                        
+                        Button("#history") {
+                            print("Pressed")
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                        
+                    }
+                    .padding()
+                }
+                
+                
+                ScrollView {
+                    VStack {
+                        Button("What is the capital of Italy?") {
+                            
+                        }
+                        .padding()
+                        .font(.system(size: 35.0, weight: .bold,design: .rounded))
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .multilineTextAlignment(.center)
+                        .cornerRadius(15)
+                        .navigationTitle("Edit Stack")
+                        .navigationBarTitleDisplayMode(.inline)
+                        
+                        Button("What is the capital of Italy?") {
+                            
+                        }
+                        .padding()
+                        .font(.system(size: 35.0, weight: .bold,design: .rounded))
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .multilineTextAlignment(.center)
+                        .cornerRadius(15)
+                        .navigationTitle("Edit Stack")
+                        
+                        Button("What is the capital of Italy?") {
+                            
+                        }
+                        .padding()
+                        .font(.system(size: 35.0, weight: .bold,design: .rounded))
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .multilineTextAlignment(.center)
+                        .cornerRadius(15)
+                        .navigationTitle("Edit Stack")
+                        
+                        Button("What is the capital of Italy?") {
+                            
+                        }
+                        .padding()
+                        .font(.system(size: 35.0, weight: .bold,design: .rounded))
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .multilineTextAlignment(.center)
+                        .cornerRadius(15)
+                        .navigationTitle("Edit Stack")
+                        Button("What is the capital of Italy?") {
+                            
+                        }
+                        .padding()
+                        .font(.system(size: 35.0, weight: .bold,design: .rounded))
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .multilineTextAlignment(.center)
+                        .cornerRadius(15)
+                        .navigationTitle("Edit Stack")
+                    }
+                }
+                
+            }
+            .toolbar {
+                ToolbarItem(placement: ToolbarItemPlacement.navigation) {
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                    }
+                    
+                }
+                
+                ToolbarItem(placement: ToolbarItemPlacement.automatic) {
+                    
+                    Button("Done") {
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct CustomTextEditor: View {
+    
+    let placeholder: String
+    @Binding var text: String
+    let internalPadding: CGFloat = 5
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(Color.primary.opacity(0.25))
+                    .fontWeight(.bold)
+                    .font(.system(size: 25))
+                    .accentColor(.green)
+                    .padding(EdgeInsets(top: 7, leading: 4, bottom: 0, trailing: 0))
+                    .padding(internalPadding)
+                
+            }
+            TextEditor(text: $text)
+                .font(.system(size: 24.0, weight: .bold,design: .rounded))
+                .lineLimit(1)
+                .padding(internalPadding)
+            
+        }.onAppear() {
+            UITextView.appearance().backgroundColor = UIColor(red: 204/255, green: 229/255, blue: 255/255, alpha: 0)
+        }.onDisappear() {
+            UITextView.appearance().backgroundColor = UIColor(red: 204/255, green: 229/255, blue: 255/255, alpha: 0)
+        }
     }
 }
 
